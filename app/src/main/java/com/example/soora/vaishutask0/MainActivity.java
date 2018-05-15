361f9fdb710010e49a06168c2322f7ed842b6835
vaishnaviikv <vaishutps@gmail.com>
2018-05-15 22:07:02 +0530

    test task 1

--- app/src/main/java/com/example/soora/vaishutask0/MainActivity.java
+++ app/src/main/java/com/example/soora/vaishutask0/MainActivity.java
@@ -11,17 +11,22 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    ConstraintLayout bg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bg = (ConstraintLayout) findViewById(R.id.bg);
     }
 int sum,n;
+    
     public void generate(View view) {
         sum=0;
         Random rand = new Random();
         n = rand.nextInt(100) + 1;
-
+        bg.setBackgroundColor(Color.WHITE);
         TextView myText = (TextView) findViewById(R.id.textView);
         String myString = String.valueOf(n);
         myText.setText(myString);
@@ -33,55 +38,52 @@ int sum,n;
 
     public void add1(View view)
     { sum= sum+1;
-        ConstraintLayout bg;
-        if(sum==n) {
-            bg = (ConstraintLayout) findViewById(R.id.bg);
-            bg.setBackgroundColor(Color.WHITE);
 
-        }
-
-       TextView myText = (TextView) findViewById(R.id.textView2);
-        String myString = String.valueOf(sum);
-        myText.setText(myString);
-
-    }
+       changebg(sum);
+       }
     public void add2(View view)
-    { sum= sum+2;ConstraintLayout bg;
-        if(sum==n) {
-            bg = (ConstraintLayout) findViewById(R.id.bg);
-            bg.setBackgroundColor(Color.WHITE);
+    { sum= sum+2;
+       changebg(sum);
 
-        }
-        TextView myText = (TextView) findViewById(R.id.textView2);
-        String myString = String.valueOf(sum);
-        myText.setText(myString);
+       }
+       public void add5(View view)
+    { sum= sum+5;
+       changebg(sum);
 
-    } public void add5(View view)
-    { sum= sum+5;ConstraintLayout bg;
-        if(sum==n) {
-            bg = (ConstraintLayout) findViewById(R.id.bg);
-            bg.setBackgroundColor(Color.WHITE);
-
-        }
-        TextView myText = (TextView) findViewById(R.id.textView2);
-        String myString = String.valueOf(sum);
-        myText.setText(myString);
 
     }
     public void add10(View view)
-     { ConstraintLayout bg;
-         if(sum==n) {
-             bg = (ConstraintLayout) findViewById(R.id.bg);
-             bg.setBackgroundColor(Color.WHITE);
+     {
+     sum= sum+10;
+     changebg(sum);
 
-         }
-        TextView myText = (TextView) findViewById(R.id.textView2);
-        String myString = String.valueOf(sum);
-        myText.setText(myString);
+
 
     }
     public void reset(View view)
     { sum= 0;
+        TextView myText = (TextView) findViewById(R.id.textView2);
+        String myString = String.valueOf(sum);
+        myText.setText(myString);
+
+    }
+    public void dec(View view)
+    {
+        sum=sum-1;
+        TextView myText = (TextView) findViewById(R.id.textView2);
+        String myString = String.valueOf(sum);
+        myText.setText(myString);
+    }
+
+    private void changebg(int num)
+    {
+        if(num==n) {
+
+
+            bg.setBackgroundColor(Color.argb(255,255,69,0));
+
+        }
+
         TextView myText = (TextView) findViewById(R.id.textView2);
         String myString = String.valueOf(sum);
         myText.setText(myString);
